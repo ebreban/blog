2055c16179869067bdb9b4996065e411899feee6
Ethan Breban <ethanbreban@Ethans-Air.lan>
2024-09-14 22:52:42 -0400

    analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
@@ -9,5 +10,10 @@ export default defineConfig({
   base: '/blog/',
   integrations: [
     tailwind(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+  }),
  ],
 });
